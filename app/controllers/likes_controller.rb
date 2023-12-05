400f938fe6dfbc6a0e7463ff3ce41e03acbeb5fc
Pedro Lizarraga <plizarraga86@gmail.com>
2023-12-04 16:22:11 -0800

    Make sure liking other users posts works. And the likes show even after refreshing the page.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @post = current_user.posts.find_by(id: params[:post_id])
+    @post = Post.find(params[:post_id])
     current_user.likes.where(post_id: @post.id).delete_all
   end
 end
